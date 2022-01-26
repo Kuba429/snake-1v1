b53ffbf4daadeb77a8d301732a3cfe88f7a25ef1
Kuba429 <jakubzawadzki429@gmail.com>
2022-01-26 15:27:42 +0100

    room status enhancement

--- src/RoomTracker.ts
+++ src/RoomTracker.ts
@@ -5,8 +5,21 @@ class RoomTracker {
     constructor() {
         this.rooms = {};
     }
-    getRoomStatus(roomId) {
-        return io.sockets.adapter.rooms?.get(roomId)?.size;
+    getRoomStatus(roomId: string): number {
+        if (!roomId) {
+            return 0;
+        }
+        const size: number = io.sockets.adapter.rooms?.get(roomId)?.size || 0;
+        this.rooms[roomId] = size;
+        this.clearEmptyRooms();
+        return size;
+    }
+    clearEmptyRooms() {
+        for (let item in this.rooms) {
+            if (this.rooms[item] < 1) {
+                delete this.rooms[item];
+            }
+        }
     }
 }
 export default RoomTracker;
