65ccafb24e4494d255f41a35dfd58744ca5b86cb
Kuba429 <jakubzawadzki429@gmail.com>
2022-01-27 12:57:37 +0100

    working with username

--- client/src/landingPage.ts
+++ client/src/landingPage.ts
@@ -1,20 +1,59 @@
 import "./styles/landingPage.scss";
 import axios from "axios";
 
-const usernameForm = document.querySelector("form#usernameForm");
+const usernameForm = <HTMLFormElement>(
+    document?.querySelector("form#usernameForm")
+);
+const usernameInput: HTMLInputElement =
+    document.querySelector(".usernameInput")!;
+const storedUsername = localStorage.getItem("username");
+usernameInput!.value =
+    storedUsername !== "guest" && storedUsername ? storedUsername : "";
 usernameForm?.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(<HTMLFormElement>e.target);
-    localStorage.setItem("username", <string>formData.get("username"));
+
+    localStorage.setItem(
+        "username",
+        <string>formData.get("username") || "guest"
+    );
 });
-const getActiveRooms = async () => {
+const getActiveRooms = async (): Promise<object> => {
     let activeRooms = await axios({
         baseURL: "http://localhost:5000",
         url: "/activeRooms",
         method: "get",
         headers: { "Content-Type": "application/json" },
     });
-    console.log(activeRooms.data);
     return activeRooms.data;
 };
 
+const init = async () => {
+    const roomList = document.querySelector(".availableRooms");
+    const rooms: object = await getActiveRooms();
+    for (let item in rooms) {
+        roomList?.appendChild(createListItem(<keyof object>item, rooms));
+    }
+};
+
+init();
+function createListItem<T extends keyof object>(item: T, object: object) {
+    // anchor tag
+    let a = document.createElement("a");
+    a.href = `${document.location.origin}/r/${item}`;
+    a.textContent = item;
+    a.addEventListener("click", (e) => {
+        e.preventDefault();
+        usernameForm?.dispatchEvent(new Event("submit"));
+        location.replace(a.href);
+    });
+    const span = document.createElement("span");
+    span.textContent = object[item];
+
+    let li = document.createElement("li");
+
+    a.appendChild(span);
+    li.appendChild(a);
+    li.classList.add("roomElement");
+    return li;
+}
