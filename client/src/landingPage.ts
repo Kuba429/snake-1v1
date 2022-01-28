fae4f5c6e35a4c03700f9b495350c0ac971691db
Kuba429 <jakubzawadzki429@gmail.com>
2022-01-28 11:54:29 +0100

    go to room on submit

--- client/src/landingPage.ts
+++ client/src/landingPage.ts
@@ -4,13 +4,20 @@ import axios from "axios";
 const usernameForm = <HTMLFormElement>(
 	document?.querySelector("form#usernameForm")
 );
-const roomInput = document.querySelector(".roomInput")!;
+const roomForm = document.querySelector("#roomForm")!;
 const usernameInput: HTMLInputElement =
 	document.querySelector(".usernameInput")!;
 
 const storedUsername = localStorage.getItem("username");
 usernameInput!.value =
 	storedUsername !== "guest" && storedUsername ? storedUsername : "";
+
+roomForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const formData = new FormData(<HTMLFormElement>e.target);
+	const roomPath = formData.get("room");
+	location.replace(`${document.location.origin}/r/${roomPath}`);
+});
 
 usernameForm?.addEventListener("submit", (e) => {
 	e.preventDefault();
@@ -21,6 +28,7 @@ usernameForm?.addEventListener("submit", (e) => {
 		<string>formData.get("username") || "guest"
 	);
 });
+
 const getActiveRooms = async (): Promise<object> => {
 	let activeRooms = await axios({
 		baseURL: "http://localhost:5000",
@@ -35,7 +43,7 @@ const listRooms = async () => {
 	const roomList = document.querySelector(".availableRooms");
 	//remove all current children in order to avoid listing a room twice
 	while (roomList?.firstChild) roomList.removeChild(roomList.firstChild);
-	roomList?.appendChild(roomInput);
+	roomList?.appendChild(roomForm);
 	const rooms: object = await getActiveRooms();
 	for (let item in rooms) {
 		roomList?.appendChild(createListItem(<keyof object>item, rooms));
