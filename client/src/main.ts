0e12c525048250df4c88a36edb2bc4290ea3c963
Kuba429 <jakubzawadzki429@gmail.com>
2022-01-29 16:28:10 +0100

    canvas

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,3 +2,5 @@ import SocketHandler from "./SocketHandler";
 import "./styles/index.scss";
 
 export const socket = new SocketHandler();
+const canvas: HTMLCanvasElement = document.querySelector("#mainCanvas")!;
+const ctx = canvas.getContext("2d");
