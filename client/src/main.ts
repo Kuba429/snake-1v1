704c438956d612d59e343cae18cc2997071c294e
Kuba429 <jakubzawadzki429@gmail.com>
2022-01-29 17:24:26 +0100

    ts variable never used err

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,3 +4,4 @@ import "./styles/index.scss";
 export const socket = new SocketHandler();
 const canvas: HTMLCanvasElement = document.querySelector("#mainCanvas")!;
 const ctx = canvas.getContext("2d");
+console.log(ctx);
