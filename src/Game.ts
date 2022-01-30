26e9dad08a5e9222f509a67603ceee755d9a009c
Kuba429 <jakubzawadzki429@gmail.com>
2022-01-30 17:51:47 +0100

    socket interface fix, some initial values

--- src/Game.ts
+++ src/Game.ts
@@ -1,16 +1,26 @@
-import { SocketType } from "dgram";
-import { Socket } from "socket.io";
-
+interface Socket {
+	on(event: string, callback: (data: any) => void);
+	emit(event: string, data: any);
+}
 export class Game {
 	roomId: string;
-	sockets: Array<SocketType>;
-	p1: SocketType;
-	p2: SocketType;
-	constructor(sockets: Array<SocketType>, roomId: string) {
+	sockets: Array<Socket>;
+	gridSize: number;
+	p1: Socket;
+	p2: Socket;
+	interval: typeof setInterval;
+	constructor(sockets: Array<Socket>, roomId: string) {
 		this.roomId = roomId;
 		this.sockets = sockets;
+		this.gridSize = 30;
 		this.p1 = this.sockets[0];
 		this.p2 = this.sockets[1];
+		this.interval = undefined;
+		this.setupListeners();
+		this.start();
+	}
+	start() {
+		this.sockets.forEach((socket) => {});
 	}
 	setupListeners() {
 		this.sockets.forEach((socket) => {
