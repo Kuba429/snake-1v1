deefd862662f8583fc555a987a5c2b064de7398f
Kuba429 <jakubzawadzki429@gmail.com>
2022-01-30 18:51:56 +0100

    interval

--- src/Game.ts
+++ src/Game.ts
@@ -10,21 +10,34 @@ export class Game {
 	gridSize: number;
 	p1: Player;
 	p2: Player;
-	interval: typeof setInterval;
+	interval: ReturnType<typeof setTimeout>;
 	fps: number;
 	constructor(sockets: Array<Socket>, roomId: string) {
 		this.roomId = roomId;
 		this.sockets = sockets;
 		this.gridSize = 30;
-		this.p1 = new Player(this.sockets[0], 0, 2, this.gridSize);
-		this.p2 = new Player(this.sockets[1], 0, 2, this.gridSize);
+		this.p1 = new Player(this.sockets[0], 1, 10, this.gridSize);
+		this.p2 = new Player(this.sockets[1], 29, 10, this.gridSize);
 		this.interval = undefined;
 		this.fps = 1;
 		this.setupListeners();
 		this.start();
 	}
 	start() {
-		this.sockets.forEach((socket) => {});
+		this.interval = setInterval(() => {
+			this.executeFrame();
+		}, 1000 / this.fps);
+		console.log(this.p1);
+		console.log(this.p2);
+	}
+	executeFrame() {
+		this.p1.move();
+		this.p2.move();
+
+		const p1Pos = { x: this.p1.x, y: this.p1.y, tail: this.p1.tail };
+		const p2Pos = { x: this.p2.x, y: this.p2.y, tail: this.p2.tail };
+		this.p1.socket.emit("update", { you: p1Pos, enemy: p2Pos });
+		this.p2.socket.emit("update", { you: p2Pos, enemy: p1Pos });
 	}
 	setupListeners() {
 		this.sockets.forEach((socket) => {
