4c8f5d2b00d698dc771c3742d959a38f8c5a1ea4
Kuba429 <jakubzawadzki429@gmail.com>
2022-02-02 16:32:50 +0100

    move method

--- client/src/Snake.ts
+++ client/src/Snake.ts
@@ -1,3 +1,4 @@
+import { game } from "./main";
 interface block {
 	x: number;
 	y: number;
@@ -10,6 +11,7 @@ export class Snake {
 	ready: boolean;
 	queue: string;
 	tail: Array<block>;
+	isOnline: boolean;
 	constructor(color: string, x: number, y: number) {
 		this.color = color;
 		this.direction = "ArrowUp";
@@ -21,5 +23,77 @@ export class Snake {
 			{ x: this.x, y: this.y },
 			{ x: this.x, y: this.y },
 		];
+		this.isOnline = true;
 	}
+	move() {
+		// tail
+		this.tail[this.tail.length - 1].x = this.x;
+		this.tail[this.tail.length - 1].y = this.y;
+		for (let i = 0; i < this.tail.length - 1; i++) {
+			this.tail[i] = { ...this.tail[i + 1] };
+		}
+		// head
+
+		switch (this.direction) {
+			case "ArrowUp":
+				this.y -= game.cellSize;
+				break;
+			case "ArrowDown":
+				this.y += game.cellSize;
+				break;
+			case "ArrowRight":
+				this.x += game.cellSize;
+				break;
+			case "ArrowLeft":
+				this.x -= game.cellSize;
+				break;
+			default:
+				return;
+		}
+		const limit = (game.gridSize - 1) * game.cellSize;
+		if (this.x > limit) {
+			this.x = 0;
+		} else if (this.x < 0) {
+			this.x = limit;
+		}
+		if (this.y > limit) {
+			this.y = 0;
+		} else if (this.y < 0) {
+			this.y = limit;
+		}
+
+		if (
+			this.queue != this.direction &&
+			getOpposite(this.queue) != this.direction
+		) {
+			this.direction = this.queue;
+		}
+		this.ready = true;
+		this.draw();
+		this.detectCollision();
+	}
+	draw() {}
+	detectCollision() {}
 }
+
+const getOpposite = (direction: string) => {
+	let toReturn: string = undefined!;
+	switch (direction) {
+		case "ArrowUp":
+			toReturn = "ArrowDown";
+			break;
+		case "ArrowDown":
+			toReturn = "ArrowUp";
+			break;
+		case "ArrowLeft":
+			toReturn = "ArrowRight";
+			break;
+		case "ArrowRight":
+			toReturn = "ArrowLeft";
+			break;
+		default:
+			break;
+	}
+
+	return toReturn;
+};
