ee523d2cbfa3e38cd680fe011a3d972989872d50
Kuba429 <jakubzawadzki429@gmail.com>
2022-02-06 13:24:05 +0100

    removed Player class from the server

--- src/Game.ts
+++ src/Game.ts
@@ -1,5 +1,3 @@
-import { Player } from "./Player";
-
 export interface Socket {
 	on(event: string, callback: (data: any) => void);
 	emit(event: string, data: any);
@@ -9,25 +7,17 @@ export class Game {
 	roomId: string;
 	sockets: Array<Socket>;
 	gridSize: number;
-	p1: Player;
-	p2: Player;
+	p1: Socket;
+	p2: Socket;
 	interval: ReturnType<typeof setTimeout>;
 	fps: number;
 	constructor(sockets: Array<Socket>, roomId: string) {
 		this.roomId = roomId;
 		this.sockets = sockets;
-		this.gridSize = 30;
-		this.p1 = new Player(this.sockets[0], 1, 10, this.gridSize);
-		this.p2 = new Player(this.sockets[1], 29, 10, this.gridSize);
-		this.interval = undefined;
+		this.p1 = this.sockets[0];
+		this.p2 = this.sockets[1];
 		this.fps = 7;
 		this.setupListeners();
-		this.start();
-	}
-	start() {}
-	executeFrame() {
-		this.p1.socket.emit("update", "a");
-		this.p2.socket.emit("update", "a");
 	}
 	setupListeners() {
 		this.sockets.forEach((socket) => {
