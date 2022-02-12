18d154dd4ce2f3b7e1255bc1c062224feedc5ea9
Kuba429 <jakubzawadzki429@gmail.com>
2022-02-12 12:58:24 +0100

    arrow positioning

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,8 @@ import { Food } from "./Food";
 import { Game } from "./Game";
 import SocketHandler from "./SocketHandler";
 import "./styles/index.scss";
+import { convertImages } from "./assets/convertToSvg";
+convertImages(".arrow img");
 
 export const socket = new SocketHandler();
 export const game = new Game();
