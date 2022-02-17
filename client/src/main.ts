eab3d740894b483f886d32caa97d5001756896d8
Kuba429 <jakubzawadzki429@gmail.com>
2022-02-17 16:59:25 +0100

    information screen styling&layout

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,8 +3,10 @@ import { Game } from "./Game";
 import SocketHandler from "./SocketHandler";
 import "./styles/index.scss";
 import { convertImages } from "./assets/convertToSvg";
+import { InfoScreen } from "./InfoScreen";
 convertImages(".arrow img");
 
 export const socket = new SocketHandler();
 export const game = new Game();
 export const food: Food = new Food(500, 500);
+export const info: InfoScreen = new InfoScreen();
